17a6c0b8caf2ba6a0e203bfd2eb194b8a01ac009
flanagas2_test2@hotmail.com <flanagas2_test2@hotmail.com>
2022-02-28 16:30:00 -0800

    Initial commit

---
+++ ChristmasShoppingCart/ChristmasShoppingCart/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ChristmasShoppingCart.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChristmasShoppingCart
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new ShoppingCartView();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ChristmasShoppingCart/ChristmasShoppingCart/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,44 @@
+﻿using ChristmasShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ChristmasShoppingCart.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public ObservableCollection<Item> Items { get; set; }
+
+        public ShoppingCartViewModel()
+        {
+            Items = new ObservableCollection<Item>
+            {
+                new Item
+                {
+                    Picture="Details1",
+                    Title = "Corgi Bear Sewing Supplies",
+                    By ="By Fluffmonger",
+                    Quantity = "x1",
+                    Price = "$50.99"
+                },
+                new Item
+                {
+                    Picture="Details2",
+                    Title = "Reindeer Crochet Hat",
+                    By ="By EmmasToys",
+                    Quantity = "x1",
+                    Price = "$30.87"
+                },
+                new Item
+                {
+                    Picture="Details3",
+                    Title = "Reindeer Socks Power Blue",
+                    By ="By TheWarmCats",
+                    Quantity = "x1",
+                    Price = "$8.56"
+                }
+            };
+        }
+    }
+}
